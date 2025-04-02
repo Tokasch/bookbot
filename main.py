52256137959216c607a79794186098edfb62e8ca
Tokasch <t-schulz@live.de>
2025-04-02 14:33:37 +0200

    updated to new boot.dev cli

--- main.py
+++ main.py
@@ -1,46 +1,39 @@
+from stats import (
+    get_num_words,
+    chars_dict_to_sorted_list,
+    get_chars_dict,
+)
+
+
 def main():
-    
-    from stats import get_num_words
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    chars_dict = count_character_frequency(text)
-    print_report(book_path, num_words, chars_dict)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+    print_report(book_path, num_words, chars_sorted_list)
 
-
-        
-def count_character_frequency(text):        
-        freq = {}
-        for c in text:
-            lowered = c.lower()
-            if lowered in freq:
-                freq[lowered] += 1
-            else:
-                freq[lowered] = 1
-        return(freq)  
-
-def sort_on(dict):
-    return dict["num"]  
-
-def print_report(path, num_words, char_dict):
-    print(f"--- Begin report of {path} ---")    
-    print(f"{num_words} words found in the document\n")    
-
-    chars_list = []
-
-    for char, count in char_dict.items():
-        if char.isalpha():
-            chars_list.append({"char": char, "num": count}) 
-
-    chars_list.sort(reverse=True, key=sort_on)
-
-    for char_info in chars_list:
-        print(f"The '{char_info['char']}' character was found {char_info['num']} times")        
 
 def get_book_text(path):
     with open(path) as f:
-        return f.read()   
+        return f.read()
 
-main() 
+
+def print_report(book_path, num_words, chars_sorted_list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"{item['char']}: {item['num']}")
+
+    print("============= END ===============")
+
+
+main()
 
 
 
